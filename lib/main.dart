be4c3d89c5ae00e7175ac387cbfb074eaa0b1280
Westefns Damião de Souza Silva <l4304@liga.org.br>
2021-02-11 14:09:59 -0300

    Aula Material e Cupertino

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:imc/ui/android/pages/home.page.dart';
+import 'package:imc/ui/android/material-app.dart';
+// import 'package:imc/ui/ios/cupertino-app.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyMaterialApp());
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: HomePage(),
-    );
-  }
-}
+// void main() {
+//   runApp(MyCupertinoApp());
+// }
