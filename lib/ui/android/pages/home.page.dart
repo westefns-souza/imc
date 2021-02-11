40512945d7370c9ac6060bed046ea9f9bd713106
Westefns Damião de Souza Silva <l4304@liga.org.br>
2021-02-11 14:00:30 -0300

    Aula Tela do Android

--- lib/ui/android/pages/home.page.dart
+++ lib/ui/android/pages/home.page.dart
@@ -8,6 +8,52 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Cálculo de IMC"),
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: TextFormField(
+              decoration: InputDecoration(
+                labelText: "Altura (cm)",
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: TextFormField(
+              decoration: InputDecoration(
+                labelText: "Peso (kg)",
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              "Você está fora de forma!",
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: FlatButton(
+              color: Theme.of(context).primaryColor,
+              child: Text(
+                "Calcular",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
