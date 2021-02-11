3b8b2bf44c20550a1f6c0c04c51508140c9c4a15
Westefns Damião de Souza Silva <l4304@liga.org.br>
2021-02-11 14:42:35 -0300

    Aula Chaveando entre SOs

--- lib/blocs/imc.bloc.dart
+++ lib/blocs/imc.bloc.dart
@@ -14,7 +14,9 @@ class ImcBloc {
       result = "Abaixo do Peso (${imc.toStringAsPrecision(2)})";
     } else if (imc >= 18.6 && imc <= 24.9) {
       result = "Peso ideal (${imc.toStringAsPrecision(2)})";
-    } else if (imc >= 24.9 && imc <= 34.9) {
+    } else if (imc >= 24.9 && imc <= 29.9) {
+      result = "Levemente acima do peso (${imc.toStringAsPrecision(2)})";
+    } else if (imc >= 29.9 && imc <= 34.9) {
       result = "Obesidade Grau I (${imc.toStringAsPrecision(2)})";
     } else if (imc >= 34.9 && imc <= 39.9) {
       result = "Obesidade Grau II (${imc.toStringAsPrecision(2)})";
